a921799e7f283fc3c010b4d56189bed4992ca7ea
Zackaria Antar <antarzack@gmail.com>
2023-05-02 14:19:58 -0500

    figured out logic to isolate name and bonus percentage.

--- client.js
+++ client.js
@@ -48,10 +48,33 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+function calculateIndividualEmployeeBonus( employee ) {
   // your logic here
-  
-  
+  newReview = {};
+
+  let bonusPercentage = 0;
+  if (employee.reviewRating === 3) {
+	  bonusPercentage = 0.04;
+  } else if (employee.reviewRating === 4) {
+	  bonusPercentage = 0.06;
+  } else if (employee.reviewRating === 5 ){
+    bonusPercentage = .1
+  }
+
+
+  newReview['name']= employee.name
+  newReview['bonusPercentage']= bonusPercentage
+return newReview;
+}
+
   // return new object with bonus results
 
-}
+
+
+calculateIndividualEmployeeBonus(employees[2]);
+console.log(newReview);
+
+
+// for (worker of employees){
+//   calculateIndividualEmployeeBonus(worker)
+// }
