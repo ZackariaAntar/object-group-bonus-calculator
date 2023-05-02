753c1791dcbc89764cde8696b5c7c2fa4a96a579
Zackaria Antar <antarzack@gmail.com>
2023-05-02 15:25:17 -0500

    changed the way we returned our object.

--- client.js
+++ client.js
@@ -1,39 +1,38 @@
 // array of employee objects
 const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+	{
+		name: "Atticus",
+		employeeNumber: "2405",
+		annualSalary: "47000",
+		reviewRating: 3,
+	},
+	{
+		name: "Jem",
+		employeeNumber: "62347",
+		annualSalary: "63500",
+		reviewRating: 4,
+	},
+	{
+		name: "Scout",
+		employeeNumber: "6243",
+		annualSalary: "74750",
+		reviewRating: 5,
+	},
+	{
+		name: "Robert",
+		employeeNumber: "26835",
+		annualSalary: "66000",
+		reviewRating: 1,
+	},
+	{
+		name: "Mayella",
+		employeeNumber: "89068",
+		annualSalary: "35000",
+		reviewRating: 1,
+	},
 ];
 
 // console.log('array of employee data: ',  employees );
-
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
@@ -43,49 +42,48 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-
-
-
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {
-  // your logic here
-  newReview = {};
+function calculateIndividualEmployeeBonus(employee) {
+	// your logic here
+	//let newReview = {};
+	//created a newReview object
+	// declared our variable bonusPercentage and set up our if statments
 
-  let bonusPercentage = 0;
-  if (employee.reviewRating === 3) {
-	  bonusPercentage = 0.04;
-  } else if (employee.reviewRating === 4) {
-	  bonusPercentage = 0.06;
-  } else if (employee.reviewRating === 5 ){
-    bonusPercentage = .1
+	let bonusPercentage = 0;
+	if (employee.reviewRating === 3) {
+		bonusPercentage = 0.04;
+	} else if (employee.reviewRating === 4) {
+		bonusPercentage = 0.06;
+	} else if (employee.reviewRating === 5) {
+		bonusPercentage = 0.1;
+	}
+
+	if (employee.employeeNumber.length === 4) {
+		bonusPercentage += 0.05;
+	}
+	if (employees.annualSalary > 65000) {
+		bonusPercentage -= 0.01;
+	}
+	if (bonusPercentage > 0.13) {
+		bonusPercentage = 0.13;
+	}
+	//declared a new variable called increase that is 1 plus 'bonusPercentage'
+	//created new objects keys and values for newReview object
+	let increase = 1 + bonusPercentage;
+	return {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+	  totalCompensation: employee.annualSalary * increase,
+	  totalBonus: Math.round(employee.annualSalary * bonusPercentage),
+    }
   }
 
-  if (employee.employeeNumber.length === 4){
-    bonusPercentage+=.05
-  }
-  if(employees.annualSalary>65000){
-    bonusPercentage -= .01
-  }
-if(bonusPercentage>.13){
-  bonusPercentage = .13
+// return new object with bonus results
+
+// calculateIndividualEmployeeBonus(employees[2]);
+// console.log(newReview);
+
+for (worker of employees) {
+	console.log(calculateIndividualEmployeeBonus(worker));
 }
-let increase = 1 + bonusPercentage
-  newReview['name']= employee.name
-  newReview['bonusPercentage']= bonusPercentage
-  newReview['totalCompensation']= employee.annualSalary * increase
-  newReview["totalBonus"] = Math.round(employee.annualSalary * bonusPercentage);
-return newReview;
-}
-
-  // return new object with bonus results
-
-
-
-calculateIndividualEmployeeBonus(employees[2]);
-console.log(newReview);
-
-
-// for (worker of employees){
-//   calculateIndividualEmployeeBonus(worker)
-// }
